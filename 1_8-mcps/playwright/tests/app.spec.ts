f946205730f40a4e297c75dac194654540746c27
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2026-04-12 20:53:52 -0300

    test: fix first tests

--- 1_8-mcps/playwright/tests/app.spec.ts
+++ 1_8-mcps/playwright/tests/app.spec.ts
@@ -1,14 +1,18 @@
 import { expect, test } from "@playwright/test";
 
 test("home page loads and displays todos header", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle(/vanilla/i);
+  await page.goto("./");
+  await expect(page).toHaveTitle(/TDD Frontend Example/i);
 });
 
-test("can add a new todo item", async ({ page }) => {
-  await page.goto("/");
-  const input = page.locator('input[type="text"]');
-  await input.fill("Buy milk");
-  await input.press("Enter");
-  await expect(page.locator("ul li")).toContainText("Buy milk");
+test("can add a new item", async ({ page }) => {
+  await page.goto("./");
+  await page.getByRole("textbox", { name: "Image Title" }).fill("Test Image");
+  await page
+    .getByRole("textbox", { name: "Image URL" })
+    .fill("https://picsum.photos/200");
+  await page.getByRole("button", { name: "Submit Form" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Test Image", level: 4 }),
+  ).toBeVisible();
 });
